ed08932baa2d189640bc9b31562e2d36f8cce2a2
Jake Bruun <jake.bruun@gmail.com>
2019-03-07 16:46:43 -0800

    Fleshes out the data model for the PlaysResponse.

--- src/BoardGamer.BoardGameGeek/BoardGameGeekXmlApi2/PlaysResponse.cs
+++ src/BoardGamer.BoardGameGeek/BoardGameGeekXmlApi2/PlaysResponse.cs
@@ -1,16 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BoardGamer.BoardGameGeek.BoardGameGeekXmlApi2
 {
     public class PlaysResponse
     {
-        public PlaysResponse()
+        public PlaysResponse(PlaysCollection plays)
         {
+            this.Result = plays;
             this.Succeeded = true;
         }
 
+        public PlaysCollection Result { get; }
         public bool Succeeded { get; }
+
+
+        public class PlaysCollection : List<Play>
+        {
+            public PlaysCollection()
+            {
+
+            }
+
+            public PlaysCollection(IEnumerable<Play> collection)
+                : base(collection)
+            { }
+
+            public string Username { get; set; }
+            public int UserId { get; set; }
+            public int Total { get; set; }
+            public int Page { get; set; }
+        }
+
+        public class Play
+        {
+            public int Id { get; set; }
+            public DateTime Date { get; set; }
+            public int Quanity { get; set; }
+            public int Length { get; set; }
+            public bool Incomplete { get; set; }
+            public bool NowInStats { get; set; }
+            public string Location { get; set; }
+            public Item Item { get; set; }
+            public string Comments { get; set; }
+            public List<Player> Players { get; set; }
+        }
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public int UserId { get; set; }
+            public string ObjectType { get; set; }
+            public int ObjectId { get; set; }
+            public List<string> SubTypes { get; set; }
+        }
+
+        public class Player
+        {
+            public string Username { get; set; }
+            public int? UserId { get; set; }
+            public string Name { get; set; }
+            public string StartPosition { get; set; }
+            public string Color { get; set; }
+            public string Score { get; set; }
+            public bool New { get; set; }
+            public string Rating { get; set; }
+            public bool Win { get; set; }
+        }
     }
 }
