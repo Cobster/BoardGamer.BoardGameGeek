13d141da511e1c0bc2d6f60c72fc493e9b55e6db
Jacob Bruun <jake.bruun@gmail.com>
2019-03-09 22:44:20 -0800

    Adds support for guilds.

---
+++ src/BoardGamer.BoardGameGeek/BoardGameGeekXmlApi2/GuildResponse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoardGamer.BoardGameGeek.BoardGameGeekXmlApi2
+{
+    public class GuildResponse
+    {
+        public GuildResponse(Guild guild)
+        {
+            this.Succeeded = true;
+            this.Result = guild;
+        }
+
+        public Guild Result { get; }
+        public bool Succeeded { get; }
+
+        public class Guild
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTimeOffset Created { get; set; }
+            public string TermsOfUse { get; set; }
+            public string Category { get; set; }
+            public string Website { get; set; }
+            public string Manager { get; set; }
+            public string Description { get; set; }
+            public Location Location { get; set; }
+            public MemberCollection Members { get; set; }
+        }
+
+        public class Location
+        {
+            public string Addr1 { get; set; }
+            public string Addr2 { get; set; }
+            public string City { get; set; }
+            public string StateOrProvince { get; set; }
+            public string PostalCode { get; set; }
+            public string Country { get; set; }
+        }
+
+        public class MemberCollection : List<Member>
+        {
+            public MemberCollection(IEnumerable<Member> members)
+                : base(members)
+            {}
+
+            public int Total { get; set; }
+            public int Page { get; set; }
+        }
+
+        public class Member
+        {
+            public string Name { get; set; }
+            public DateTimeOffset Date { get; set; }
+        }
+    }
+}
