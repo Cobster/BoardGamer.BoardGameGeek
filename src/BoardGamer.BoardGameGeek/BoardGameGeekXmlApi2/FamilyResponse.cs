68237462a5e79a4048d8874ad24bd078d7b4fb0d
Jake Bruun <jake.bruun@gmail.com>
2019-03-12 10:08:35 -0700

    Adds support for families. (#41)

---
+++ src/BoardGamer.BoardGameGeek/BoardGameGeekXmlApi2/FamilyResponse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace BoardGamer.BoardGameGeek.BoardGameGeekXmlApi2
+{
+    public class FamilyResponse
+    {
+        public FamilyResponse(ItemCollection items)
+        {
+            this.Result = items;
+            this.Succeeded = true;
+        }
+
+        public ItemCollection Result { get; }
+        public bool Succeeded { get; }
+
+        public class ItemCollection : List<Item>
+        {
+            public ItemCollection(IEnumerable<Item> collection) 
+                : base(collection)
+            {}
+
+            public string TermsOfUse { get; set; }
+        }
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Type { get; set; }
+            public string Thumbnail { get; set; }
+            public string Image { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public List<Link> Links { get; set; }
+        }
+
+        public class Link
+        {
+            public int Id { get; set; }
+            public string Type { get; set; }
+            public string Value { get; set; }
+            public bool Inbound { get; set; }
+        }
+    }
+}
